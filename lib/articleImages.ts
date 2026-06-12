131570f38f45f11e3e8fd7b551c4a62873e4aff5
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-12 23:14:38 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -14,6 +14,10 @@ export function getArticleImage(
     return "/images/article-a-la-une.png";
   }
 
+  if (slug === "fiv-classique-icsi-quelles-differences") {
+    return "/images/article-a-la-une.png";
+  }
+
   if (slug === "insemination-intra-uterine-iiu-comment-ca-se-passe") {
     return "/images/article-a-la-une.png";
   }
