f2a2e1ddaf408e8000911acd6d2a29b816dcac78
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-12 22:52:04 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -14,6 +14,10 @@ export function getArticleImage(
     return "/images/article-a-la-une.png";
   }
 
+  if (slug === "pma-demarches-delais-prise-en-charge") {
+    return "/images/article-a-la-une.png";
+  }
+
   if (slug === "pma-amp-definition-a-qui-adresse") {
     return "/images/article-a-la-une.png";
   }
