58363ba0239069b3c1ff151fd0ce2db6e283a66c
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-12 23:31:56 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -14,6 +14,10 @@ export function getArticleImage(
     return "/images/article-a-la-une.png";
   }
 
+  if (slug === "comment-se-deroule-fiv-etape-par-etape") {
+    return "/images/article-a-la-une.png";
+  }
+
   if (slug === "fiv-classique-icsi-quelles-differences") {
     return "/images/article-a-la-une.png";
   }
