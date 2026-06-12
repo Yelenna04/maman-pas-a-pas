26d4a721a909710700e1fec4de2171ce7ddde2b2
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-12 22:12:13 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -14,6 +14,10 @@ export function getArticleImage(
     return "/images/article-a-la-une.png";
   }
 
+  if (slug === "alcool-projet-bebe-faut-il-arreter-avant-grossesse") {
+    return "/images/article-a-la-une.png";
+  }
+
   if (slug === "tabac-projet-grossesse-pourquoi-comment-arreter") {
     return "/images/article-a-la-une.png";
   }
