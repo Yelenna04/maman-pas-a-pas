f64386f7eba9d8313ab7cdba286220dad1807d5e
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-13 21:52:22 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -30,10 +30,6 @@ export function getArticleImage(
     return "/images/article-a-la-une.png";
   }
 
-  if (slug === "evolution-bebe-semaine-par-semaine-deuxieme-trimestre") {
-    return "/images/article-a-la-une.png";
-  }
-
   if (slug === "test-positif-premieres-demarches") {
     return "/images/article-a-la-une.png";
   }
