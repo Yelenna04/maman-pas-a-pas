58aa2766bfe90585aead80b34bc0f79f9c064488
Yelenna04 <stephanie.siccard@gmail.com>
2026-06-17 11:07:31 +0200

    Add files via upload

--- lib/articleImages.ts
+++ lib/articleImages.ts
@@ -121,27 +121,27 @@ export function getArticleImage(
   }
 
   if (slug === "comment-vivre-emotionnellement-parcours-pma") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-emotions.png";
   }
 
   if (slug === "comment-se-deroule-fiv-etape-par-etape") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-fiv-etapes.png";
   }
 
   if (slug === "fiv-classique-icsi-quelles-differences") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-fiv-icsi.png";
   }
 
   if (slug === "insemination-intra-uterine-iiu-comment-ca-se-passe") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-insemination-iiu.png";
   }
 
   if (slug === "pma-demarches-delais-prise-en-charge") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-demarches-delais.png";
   }
 
   if (slug === "pma-amp-definition-a-qui-adresse") {
-    return "/images/article-a-la-une.png";
+    return "/images/pma-parcours-definition-amp.png";
   }
 
   if (slug === "produits-quotidien-environnement-que-limiter-avant-grossesse") {
@@ -225,30 +225,6 @@ export function getArticleImage(
   }
 
 
-
-  if (slug === "comment-vivre-emotionnellement-parcours-pma") {
-    return "/images/pma-parcours-emotions.png";
-  }
-
-  if (slug === "comment-se-deroule-fiv-etape-par-etape") {
-    return "/images/pma-parcours-fiv-etapes.png";
-  }
-
-  if (slug === "fiv-classique-icsi-quelles-differences") {
-    return "/images/pma-parcours-fiv-icsi.png";
-  }
-
-  if (slug === "insemination-intra-uterine-iiu-comment-ca-se-passe") {
-    return "/images/pma-parcours-insemination-iiu.png";
-  }
-
-  if (slug === "pma-demarches-delais-prise-en-charge") {
-    return "/images/pma-parcours-demarches-delais.png";
-  }
-
-  if (slug === "pma-amp-definition-a-qui-adresse") {
-    return "/images/pma-parcours-definition-amp.png";
-  }
 
   const images: Record<string, string> = {
     "avant-grossesse": "/images/projet-bebe.png",
