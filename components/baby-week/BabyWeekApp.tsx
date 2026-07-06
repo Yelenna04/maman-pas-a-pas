3c6434ccf1200326d15a744dafaebb375e4b10ff
Yelenna04 <stephanie.siccard@gmail.com>
2026-07-06 23:58:14 +0200

    Add files via upload

--- components/baby-week/BabyWeekApp.tsx
+++ components/baby-week/BabyWeekApp.tsx
@@ -1,1 +1,32 @@
+"use client";
 
+import { useMemo, useState } from "react";
+import { babyWeeks } from "@/data/babyWeeks";
+import { BabyWeekPanel } from "./BabyWeekPanel";
+import { BabyWeekVisual } from "./BabyWeekVisual";
+import { WeekSelector } from "./WeekSelector";
+import styles from "./BabyWeekApp.module.css";
+
+export default function BabyWeekApp() {
+  const [selectedWeek, setSelectedWeek] = useState(4);
+
+  const currentWeek = useMemo(
+    () => babyWeeks.find((week) => week.week === selectedWeek) ?? babyWeeks[0],
+    [selectedWeek]
+  );
+
+  return (
+    <main className={styles.page}>
+      <section className={styles.shell}>
+        <BabyWeekPanel week={currentWeek} />
+        <BabyWeekVisual week={currentWeek} />
+      </section>
+
+      <WeekSelector
+        weeks={babyWeeks}
+        selectedWeek={selectedWeek}
+        onSelectWeek={setSelectedWeek}
+      />
+    </main>
+  );
+}
