78552a501214f3f9733e50499fb8f0cb5fee5725
Yelenna04 <stephanie.siccard@gmail.com>
2026-07-07 09:58:35 +0200

    Add files via upload

--- app/pendant-grossesse/app/pendant-grossesse/semaine-par-semaine/page.tsx
+++ app/pendant-grossesse/app/pendant-grossesse/semaine-par-semaine/page.tsx
@@ -1,1 +1,12 @@
+import SemaineParSemaineClient from "./SemaineParSemaineClient";
+import "./semaine-par-semaine.css";
 
+export const metadata = {
+  title: "Bébé semaine par semaine | Maman, Pas à Pas",
+  description:
+    "Découvrez l’évolution de votre bébé semaine après semaine, dans une interface douce et rassurante.",
+};
+
+export default function SemaineParSemainePage() {
+  return <SemaineParSemaineClient />;
+}
