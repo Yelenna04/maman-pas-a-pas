9374da80cfa0cc73e137ae8dcc244f9eb6dae38f
Yelenna04 <stephanie.siccard@gmail.com>
2026-07-07 10:02:37 +0200

    Add files via upload

---
+++ app/pendant-grossesse/semaine-par-semaine/SemaineParSemaineClient.tsx
@@ -0,0 +1,2 @@
+"use client";
+export default function SemaineParSemaineClient(){return <main><h1>Bébé semaine par semaine</h1></main>}
---
+++ app/pendant-grossesse/semaine-par-semaine/page.tsx
@@ -0,0 +1,6 @@
+import SemaineParSemaineClient from "./SemaineParSemaineClient";
+import "./semaine-par-semaine.css";
+
+export default function Page() {
+  return <SemaineParSemaineClient />;
+}
