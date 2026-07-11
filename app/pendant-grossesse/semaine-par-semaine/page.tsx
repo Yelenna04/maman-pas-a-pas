66491c3eb1d454436b4d48ac08e969b7941be658
Yelenna04 <stephanie.siccard@gmail.com>
2026-07-11 15:36:44 +0200

    Load baby week illustration styles

--- app/pendant-grossesse/semaine-par-semaine/page.tsx
+++ app/pendant-grossesse/semaine-par-semaine/page.tsx
@@ -1,6 +1,7 @@
 import SemaineParSemaineClient from "./SemaineParSemaineClient";
 import "./semaine-par-semaine.css";
 import "./semaine-par-semaine-animations.css";
+import "./illustrations.css";
 
 export default function Page() {
   return <SemaineParSemaineClient />;
