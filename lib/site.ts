c0ea31ad71269210519692c46209a2ce0fa5f9d9
Yelenna04 <stephanie.siccard@gmail.com>
2026-08-08 15:24:31 +0200

    Update contact email address

--- lib/site.ts
+++ lib/site.ts
@@ -4,5 +4,5 @@ export const siteConfig = {
   url: "https://mamanpasapas.fr",
   description:
     "Des repères fiables, doux et accessibles pour avancer avant, pendant et après la grossesse.",
-  email: "contact@mamanpasapas.fr"
+  email: "contact.mamanpasapas@gmail.com"
 };
